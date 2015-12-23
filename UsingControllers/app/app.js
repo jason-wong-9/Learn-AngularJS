0464479a56ad4191c0e59e19ce9467027f63c40e
Jason Wong <jason_19960903@hotmail.com>
2015-12-23 08:48:26 -0800

    Mistake was made

--- UsingControllers/app/app.js
+++ UsingControllers/app/app.js
@@ -1,5 +1,14 @@
 (function() {
     
-    var app = angular.module('customersApp', []);
+    var app = angular.module('customersApp', ['ngRoute']);
+
+    app.config(function ($routeProvider) {
+    	$routeProvider
+    		.when('/', {
+    			controller: 'CustomersController',
+    			templateUrl: 'app/views/customers.html'
+    		})
+    		.otherwise( { redirectTo: '/' });
+    });
     
 }());
