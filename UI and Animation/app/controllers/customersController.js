e7e40d5c87baf68e525f9a6e449d93003e3a8973
Jason Wong <jason_19960903@hotmail.com>
2015-12-23 17:06:19 -0800

    All Orders Controller

--- UI and Animation/app/controllers/customersController.js
+++ UI and Animation/app/controllers/customersController.js
@@ -1,6 +1,6 @@
 (function() {
     
-    var CustomersController = function ($scope, $log, customersFactory, appSettings) {
+    var CustomersController = function ($scope, $log, $window, customersFactory, appSettings) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
         $scope.customers = [];
@@ -22,9 +22,30 @@
            $scope.sortBy = propName;
            $scope.reverse = !$scope.reverse;
         };
+        
+        $scope.deleteCustomer = function(customerId) {
+            customersFactory.deleteCustomer(customerId)
+                .success(function(status) {
+                    if (status) {
+                        for (var i=0,len=$scope.customers.length;i<len;i++) {
+                            if ($scope.customers[i].id === customerId) {
+                               $scope.customers.splice(i,1);
+                               break;
+                            }
+                        }  
+                    }
+                    else {
+                        $window.alert('Unable to delete customer');   
+                    }
+                    
+                })
+                .error(function(data, status, headers, config) {
+                    $log.log(data.error + ' ' + status);
+                });
+        };
     };
     
-    CustomersController.$inject = ['$scope', '$log', 'customersFactory', 
+    CustomersController.$inject = ['$scope', '$log', '$window', 'customersFactory', 
                                    'appSettings'];
 
     angular.module('customersApp')
